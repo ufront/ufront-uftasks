222ed74ec01630716e4aae291a98e98996950af7
Jason O'Neil <jason.oneil@gmail.com>
2014-08-14 11:23:38 +0800

    1.0.0-beta.6

--- bundle.sh
+++ bundle.sh
@@ -3,4 +3,4 @@
 libname='ufront-uftasks'
 rm -f "${libname}.zip"
 zip -r "${libname}.zip" haxelib.json src LICENSE.txt README.md
-echo "Saved as ${libname}.zip"
+echo "Saved as ${libname}.zip. Please run git tag"
